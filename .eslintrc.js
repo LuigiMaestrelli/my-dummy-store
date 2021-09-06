aaf5ee3070dbc217802d975592843981e50821b1
Luigi Maestrelli <lgmaestrelli@gmail.com>
2021-09-06 14:13:06 -0300

    feat: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+    'next/core-web-vitals',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'prettier'
+  ],
+  rules: {
+    "prettier/prettier": ["error", { semi: true }, {
+      "usePrettierrc": true
+    }],
+    'space-before-function-paren': 'off',
+    'react/prop-types': 'off'
+  }
+}

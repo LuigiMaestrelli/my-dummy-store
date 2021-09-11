9586bb945b6ec50dce17e34c20030d6d756a3828
Luigi Maestrelli <lgmaestrelli@gmail.com>
2021-09-11 13:38:46 -0300

    feat: appBar title link

--- src/ui/components/common/AppBar/Appbar.tsx
+++ src/ui/components/common/AppBar/Appbar.tsx
@@ -1,20 +1,29 @@
 import React from 'react';
-
-import { styled } from '@mui/system';
-import { AppBar as MuiAppBar, Toolbar, Typography } from '@mui/material';
+import Link from 'next/link';
+import styled from '@mui/system/styled';
+import { ThemeOptions } from '@mui/material/styles/createTheme';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 
 import styles from './AppBar.module.css';
 
-const Offset = styled('div')(({ theme }: any) => theme.mixins.toolbar);
+type OffsetType = {
+  theme?: ThemeOptions;
+};
+
+const Offset = styled('div')(({ theme }: OffsetType) => theme?.mixins?.toolbar);
 
 export function AppBar() {
   return (
     <>
       <MuiAppBar position="fixed">
         <Toolbar>
-          <Typography variant="h6" component="div">
-            My Dummy Store
-          </Typography>
+          <Link href={'/'} passHref>
+            <Typography variant="h6" component="div" className={styles.title}>
+              My Dummy Store
+            </Typography>
+          </Link>
         </Toolbar>
       </MuiAppBar>
       <Offset className={styles.appBar} />

f508b72e0d103a377315a73cdc002b9ad12fb01e
Luigi Maestrelli <lgmaestrelli@gmail.com>
2021-09-26 13:25:30 -0300

    test: better testfor apiClient factory

--- __tests__/main/factories/infrastructure/apiClient.spec.ts
+++ __tests__/main/factories/infrastructure/apiClient.spec.ts
@@ -1,8 +1,51 @@
-import { getApiClient } from '@/main/factories/infrastructure/apiClient';
+import { getApiClient, init } from '@/main/factories/infrastructure/apiClient';
+import { makeStateManagementStub } from '@test/utils/stubs/stateManagement';
 
 describe('ApiClient Factory', () => {
-  it('should return a valid IApiClient', () => {
-    const result = getApiClient();
-    expect(result).toBeTruthy();
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  describe('getApiClient', () => {
+    it('should return a valid IApiClient', () => {
+      const result = getApiClient();
+      expect(result).toBeTruthy();
+    });
+  });
+
+  describe('init', () => {
+    it('should call stateManagement getAuthToken on init', () => {
+      const stateManagementStub = makeStateManagementStub();
+      const getAuthTokenSpy = jest.spyOn(stateManagementStub, 'getAuthToken');
+
+      init(stateManagementStub);
+
+      expect(getAuthTokenSpy).toBeCalled();
+    });
+
+    it('should call axiosApiClient setAuthToken if getAuthToken return a valid token', () => {
+      const stateManagementStub = makeStateManagementStub();
+      jest
+        .spyOn(stateManagementStub, 'getAuthToken')
+        .mockReturnValueOnce('valid token');
+      const apiClient = getApiClient();
+      const setAuthTokenSpy = jest.spyOn(apiClient, 'setAuthToken');
+
+      init(stateManagementStub);
+
+      expect(setAuthTokenSpy).toBeCalledWith('valid token');
+    });
+
+    it('should not call axiosApiClient setAuthToken if getAuthToken does not return a token', () => {
+      const stateManagementStub = makeStateManagementStub();
+      jest.spyOn(stateManagementStub, 'getAuthToken').mockReturnValueOnce('');
+
+      const apiClient = getApiClient();
+      const setAuthTokenSpy = jest.spyOn(apiClient, 'setAuthToken');
+
+      init(stateManagementStub);
+
+      expect(setAuthTokenSpy).not.toBeCalled();
+    });
   });
 });
--- src/main/factories/infrastructure/apiClient.ts
+++ src/main/factories/infrastructure/apiClient.ts
@@ -1,16 +1,19 @@
 import { IApiClient } from '@/application/protocols/apiClient';
+import { IStateManagement } from '@/application/protocols/stateManagement';
 import { AxiosApiClient } from '@/infrastructure/adapter/axiosApiClient';
 import { getStateManagement } from '@/main/factories/infrastructure/stateManagement';
 
 const axiosApiClient = new AxiosApiClient();
 
-const stateManagement = getStateManagement();
-const currentToken = stateManagement.getAuthToken();
-
-if (currentToken) {
-  axiosApiClient.setAuthToken(currentToken);
+export function init(stateManagement: IStateManagement): void {
+  const currentToken = stateManagement.getAuthToken();
+  if (currentToken) {
+    axiosApiClient.setAuthToken(currentToken);
+  }
 }
 
 export function getApiClient(): IApiClient {
   return axiosApiClient;
 }
+
+init(getStateManagement());

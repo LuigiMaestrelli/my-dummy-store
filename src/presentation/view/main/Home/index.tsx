5eb8186e2c989bd9732675ac2fa8821af9161133
Luigi Maestrelli <lgmaestrelli@gmail.com>
2021-09-27 16:54:52 -0300

    test: add tests for HomeView

--- src/presentation/view/main/Home/index.tsx
+++ src/presentation/view/main/Home/index.tsx
@@ -14,7 +14,14 @@ export default function HomeView({ products }: HomeViewProps) {
     <Container maxWidth="xl">
       <Grid container spacing={1}>
         {products.map(product => (
-          <Grid item key={product.id} xs={12} sm={6} md={3}>
+          <Grid
+            item
+            key={product.id}
+            xs={12}
+            sm={6}
+            md={3}
+            data-testid="product-grid-item"
+          >
             <ProductCard product={product} />
           </Grid>
         ))}
--- src/presentation/view/main/SignIn/index.tsx
+++ src/presentation/view/main/SignIn/index.tsx
@@ -25,10 +25,7 @@ type LoginFormDto = {
 };
 
 const SignInSchema = Yup.object().shape({
-  password: Yup.string()
-    .min(5, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Required'),
+  password: Yup.string().required('Required'),
   email: Yup.string().email('Invalid email').required('Required')
 });
 
